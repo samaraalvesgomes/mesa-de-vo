3fe58547b57b73203acf69b0e898b19dcb6e8a4b
Samara Alves Gomes <saalvesgomes11@gmail.com>
2024-12-01 15:32:39 -0300

    início da construção do navigation

--- src/pages/components/navigation/Navigation.jsx
+++ src/pages/components/navigation/Navigation.jsx
@@ -1,7 +1,41 @@
+import { Button, ButtonGroup, Link} from "@mui/material";
+
 function Navigation() {
     return (
     <>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam illo ipsam quis soluta id ipsum quaerat eius at blanditiis ex sint assumenda sapiente voluptatibus provident, consequatur, debitis, natus deserunt mollitia.</p>
+        <nav style={{background: 'RGBA(255,255,255,0.2)', display:'flex', alignItems: 'center', padding: '15px 0px', justifyContent:'center', fontSize:'15px', border: '2px solid white', minWidth:'100%'}}>
+          <h3 style={{fontFamily:'Poppins', fontSize:'20px'}}>Mesa de vó</h3>
+          
+
+          <ButtonGroup
+            variant="text"
+            aria-label="Basic button group"
+          >
+            <Link href="#scrollSobre" passHref>
+              <Button style={{ background: "none", border: "none", fontFamily: "Poppins", fontSize: "16px", textTransform: "none"}} variant="contained"> Sobre </Button>
+            </Link>
+            <Link href="/Pedido" passHref>
+              <Button style={{background:'none'}} variant="contained"> Receitas </Button>
+            </Link>
+            <Link href="/Apadrinhamento" passHref>
+              <Button style={{background:'none'}} variant="contained"> Contato </Button>
+            </Link>
+          </ButtonGroup>
+
+          <ButtonGroup
+            variant="text"
+            aria-label="Basic button group"
+          >
+            <Link href="./sign-in-side/SignInSide" passHref>
+              <Button style={{background:'blue'}} variant="contained"> Entrar </Button>
+            </Link>
+            <Link href="./sign-in-side/SignInSide" passHref>
+              <Button style={{background:'none'}} variant="contained"> Resgistre-se </Button>
+            </Link>
+          </ButtonGroup>
+
+         
+        </nav>
     </>
   );
 }
