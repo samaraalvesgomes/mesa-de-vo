b411e3848372bc332c005bc9540f4ad56cc5e63a
Samara Alves Gomes <saalvesgomes11@gmail.com>
2024-12-01 14:56:49 -0300

    Adicionando página do componente Navigation ao index

---
+++ src/pages/components/navigation/Navigation.jsx
@@ -0,0 +1,8 @@
+function Navigation() {
+    return (
+    <>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam illo ipsam quis soluta id ipsum quaerat eius at blanditiis ex sint assumenda sapiente voluptatibus provident, consequatur, debitis, natus deserunt mollitia.</p>
+    </>
+  );
+}
+export default Navigation;
