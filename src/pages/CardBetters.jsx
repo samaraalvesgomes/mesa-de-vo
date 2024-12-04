b617c40b51c6488b2ab284e113b20f6336cf66bf
Juan Pablo <juanpabloflgalvao@gmail.com>
2024-12-04 12:23:18 -0300

    Finalização da página CardBetters e alteração das dependências do projeto.

--- src/pages/CardBetters.jsx
+++ src/pages/CardBetters.jsx
@@ -1,61 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import CardRecipe from '@/pages/components/card-recipe/CardRecipe';
-import Link from 'next/link';
-import { Button } from '@mui/material';
-import styles from './testeCard.module.css'
+import styles from './CardBetters.module.css'
 
+const MOCK_RECEITAS = [
+  {
+    image: '/img/BackgroundReceitaBoloLimao.png',
+    title: 'Bolo de limão da vovó',
+    author: ' Agata Harkness',
+    difficulty: 'Fácil',
+    time: '60 min',
+  },
+  {
+    image: '/img/CupcakeBeterraba.jpg',
+    title: 'Cupcake de beterraba',
+    author: ' Megan Morse',
+    difficulty: 'Médio',
+    time: '85 min',
+  },
+  {
+    image: '/img/Pao7graos.jpg',
+    title: 'Pão 7 Grãos',
+    author: ' Shayera Hawks',
+    difficulty: 'Difícil',
+    time: '60 min',
+  },
+];
 
 const CardBetters = () => {
   const [receitas, setReceitas] = useState([]);
 
+  // Simula carregamento inicial
   useEffect(() => {
-    const fetchReceitas = async () => {
-      setTimeout(() => {
-        const mockData = [
-          {
-            image: '/img/AboboraAssada.jpg',
-            title: ' Abóbora Assada',
-            author: ' Ana Maria',
-            difficulty: ' Fácil',
-            time: ' 40 min'
-          },
-          {
-            image: '/img/Pao7graos.jpg',
-            title: ' Pão 7 Grãos',
-            author: ' Ana Maria',
-            difficulty: ' Fácil',
-            time: ' 40 min'
-          },
-          {
-            image: '/img/CookiesVeganos.jpg',
-            title: ' Cookies Veganos da vovó',
-            author: ' Ana Maria',
-            difficulty: ' Fácil',
-            time: ' 40 min'
-          },
-          {
-            image: '/img/Macarronada3queijos.jpg',
-            title: ' Macarronada 3 queijos',
-            author: ' Ana Maria',
-            difficulty: ' Fácil',
-            time: ' 40 min'
-          },
-        ];
-        setReceitas(mockData);
-      }, 1000);
+    const loadReceitas = () => {
+      setReceitas(MOCK_RECEITAS);
     };
-
-    fetchReceitas();
+    loadReceitas();
   }, []);
 
   return (
-    <div className={styles.pageContainer}>
-      <div className={styles.header}>
-        <h1>Realize um sonho!</h1>
-        <Link href="/" passHref>
-          <Button>Voltar</Button>
-        </Link>
-      </div>
+    <div className={styles.middleSection}>
+      <h1 className={styles.titleMiddle}>Melhores da Semana</h1>
       <div className={styles.cardsContainer}>
         {receitas.length > 0 ? (
           receitas.map((receita, index) => (
@@ -72,7 +56,6 @@ const CardBetters = () => {
           <p>Carregando dados...</p>
         )}
       </div>
-     
     </div>
   );
 };
